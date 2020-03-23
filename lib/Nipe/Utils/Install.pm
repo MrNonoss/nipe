93f1bd858c9c1ac2cb6eb9ea76a72771af886f19
MrNonoss <bruno.bordas@gmx.com>
2020-03-23 12:06:15 +0100

    Remove "sudo" && install systemd && force install

--- lib/Nipe/Utils/Install.pm
+++ lib/Nipe/Utils/Install.pm
@@ -6,36 +6,36 @@ use Nipe::Utils::Device;
 
 sub new {
 	my %device  = Nipe::Utils::Device -> new();
-	my $stopTor = "sudo systemctl stop tor";
+	my $stopTor = "systemctl stop tor";
 
-	system ("sudo mkdir -p /etc/tor/");
+	system ("mkdir -p /etc/tor/");
 
 	if ($device{distribution} eq "debian") {
-		system ("sudo apt-get install -y tor iptables");
-		system ("sudo cp .configs/debian-torrc /etc/tor/torrc");
+		system ("apt-get install -y tor iptables systemd -y");
+		system ("cp .configs/debian-torrc /etc/tor/torrc");
 	}
-	
+
 	elsif ($device{distribution} eq "fedora") {
-		system ("sudo dnf install -y tor iptables");
-		system ("sudo cp .configs/fedora-torrc /etc/tor/torrc");
+		system ("dnf install -y tor iptables");
+		system ("cp .configs/fedora-torrc /etc/tor/torrc");
 	}
 
 	elsif ($device{distribution} eq "centos") {
-		system ("sudo yum -y install epel-release tor iptables");
-		system ("sudo cp .configs/centos-torrc /etc/tor/torrc");
+		system ("yum -y install epel-release tor iptables");
+		system ("cp .configs/centos-torrc /etc/tor/torrc");
 	}
 
 	else {
-		system ("sudo pacman -S --noconfirm -S tor iptables");
-		system ("sudo cp .configs/arch-torrc /etc/tor/torrc");
+		system ("pacman -S --noconfirm -S tor iptables");
+		system ("cp .configs/arch-torrc /etc/tor/torrc");
 	}
 
-	system ("sudo chmod 644 /etc/tor/torrc");
+	system ("chmod 644 /etc/tor/torrc");
 
 	if (-e "/etc/init.d/tor") {
-		$stopTor = "sudo /etc/init.d/tor stop > /dev/null";
+		$stopTor = "/etc/init.d/tor stop > /dev/null";
 	}
-	
+
 	system ($stopTor);
 
 	return 1;
