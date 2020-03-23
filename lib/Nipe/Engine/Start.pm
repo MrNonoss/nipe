9ac29aa16ee36849afcb184371a86462183a9f74
MrNonoss <bruno.bordas@gmx.com>
2020-03-23 12:02:35 +0100

    Remove "sudo"

--- lib/Nipe/Engine/Start.pm
+++ lib/Nipe/Engine/Start.pm
@@ -9,16 +9,16 @@ sub new {
 	my $transferPort = "9051";
 	my @table        = ("nat", "filter");
 	my $network      = "10.66.0.0/255.255.0.0";
-	my $startTor     = "sudo systemctl start tor";
+	my $startTor     = "systemctl start tor";
 
 	my %device = Nipe::Utils::Device -> new();
 
 	if (-e "/etc/init.d/tor") {
-		$startTor = "sudo /etc/init.d/tor start > /dev/null";
+		$startTor = "/etc/init.d/tor start > /dev/null";
 	}
 
 	system ($startTor);
-	
+
 	foreach my $table (@table) {
 		my $target = "ACCEPT";
 
@@ -26,9 +26,9 @@ sub new {
 			$target = "RETURN";
 		}
 
-		system ("sudo iptables -t $table -F OUTPUT");
-		system ("sudo iptables -t $table -A OUTPUT -m state --state ESTABLISHED -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -m owner --uid $device{username} -j $target");
+		system ("iptables -t $table -F OUTPUT");
+		system ("iptables -t $table -A OUTPUT -m state --state ESTABLISHED -j $target");
+		system ("iptables -t $table -A OUTPUT -m owner --uid $device{username} -j $target");
 
 		my $matchDnsPort = $dnsPort;
 
@@ -37,33 +37,33 @@ sub new {
 			$matchDnsPort = "53";
 		}
 
-		system ("sudo iptables -t $table -A OUTPUT -p udp --dport $matchDnsPort -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -p tcp --dport $matchDnsPort -j $target");
+		system ("iptables -t $table -A OUTPUT -p udp --dport $matchDnsPort -j $target");
+		system ("iptables -t $table -A OUTPUT -p tcp --dport $matchDnsPort -j $target");
 
 		if ($table eq "nat") {
 			$target = "REDIRECT --to-ports $transferPort";
 		}
 
-		system ("sudo iptables -t $table -A OUTPUT -d $network -p tcp -j $target");
+		system ("iptables -t $table -A OUTPUT -d $network -p tcp -j $target");
 
 		if ($table eq "nat") {
 			$target = "RETURN";
 		}
 
-		system ("sudo iptables -t $table -A OUTPUT -d 127.0.0.1/8    -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 192.168.0.0/16 -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 172.16.0.0/12  -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 10.0.0.0/8     -j $target");
+		system ("iptables -t $table -A OUTPUT -d 127.0.0.1/8    -j $target");
+		system ("iptables -t $table -A OUTPUT -d 192.168.0.0/16 -j $target");
+		system ("iptables -t $table -A OUTPUT -d 172.16.0.0/12  -j $target");
+		system ("iptables -t $table -A OUTPUT -d 10.0.0.0/8     -j $target");
 
 		if ($table eq "nat") {
 			$target = "REDIRECT --to-ports $transferPort";
 		}
 
-		system ("sudo iptables -t $table -A OUTPUT -p tcp -j $target");
+		system ("iptables -t $table -A OUTPUT -p tcp -j $target");
 	}
 
-	system ("sudo iptables -t filter -A OUTPUT -p udp -j REJECT");
-	system ("sudo iptables -t filter -A OUTPUT -p icmp -j REJECT");
+	system ("iptables -t filter -A OUTPUT -p udp -j REJECT");
+	system ("iptables -t filter -A OUTPUT -p icmp -j REJECT");
 
 	return 1;
 }
