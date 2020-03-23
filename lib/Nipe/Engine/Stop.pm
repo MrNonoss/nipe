0ac49dc9a0109e6e491afbba2df19f5709766a7c
MrNonoss <bruno.bordas@gmx.com>
2020-03-23 12:02:50 +0100

    Remove "sudo"

--- lib/Nipe/Engine/Stop.pm
+++ lib/Nipe/Engine/Stop.pm
@@ -5,15 +5,15 @@ use warnings;
 
 sub new {
 	my @table   = ("nat", "filter");
-	my $stopTor = "sudo systemctl stop tor";
+	my $stopTor = "systemctl stop tor";
 
 	foreach my $table (@table) {
-		system ("sudo iptables -t $table -F OUTPUT");
-		system ("sudo iptables -t $table -F OUTPUT");
+		system ("iptables -t $table -F OUTPUT");
+		system ("iptables -t $table -F OUTPUT");
 	}
 
 	if (-e "/etc/init.d/tor") {
-		$stopTor = "sudo /etc/init.d/tor stop > /dev/null";
+		$stopTor = "/etc/init.d/tor stop > /dev/null";
 	}
 
 	system ($stopTor);
