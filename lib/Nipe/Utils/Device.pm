e3e4d29a5a79c5f1c691c9085601ee55deb683e5
MrNonoss <bruno.bordas@gmx.com>
2020-03-23 12:03:43 +0100

    Set static config

--- lib/Nipe/Utils/Device.pm
+++ lib/Nipe/Utils/Device.pm
@@ -10,24 +10,9 @@ my $id_distro = $config -> param('ID');
 
 sub new {
 	my %device = (
-		"username" => "",
-		"distribution"  => ""
+		"username" => "debian-tor",
+		"distribution"  => "debian"
 	);
-	
-	if (($id_like =~ /[F,f]edora/) || ($id_distro =~ /[F,f]edora/)) {
-		$device{username} = "toranon";
-		$device{distribution} = "fedora";
-	}
-	
-	elsif (($id_like =~ /[A,a]rch/) || ($id_like =~ /[C,c]entos/) || ($id_distro =~ /[A,a]rch/) || ($id_distro =~ /[C,c]entos/)) {
-		$device{username} = "tor";
-		$device{distribution} = "arch";
-	}
-
-	else {
-		$device{username} = "debian-tor";
-		$device{distribution} = "debian";
-	}
 
 	return %device;
 }
